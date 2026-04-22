50b3ee737217ccd8ff9839091ad10f6157fcae98
Pasala Sreya Gracy <pasalasreyagracy@gmail.com>
2026-04-22 19:45:14 +0530

    Add files via upload

---
+++ 🗄️ sql/views/Main_View.sql
@@ -0,0 +1,27 @@
+create view vw_reconciliation as
+with recon as(
+    select
+        i.order_id,
+        i.expected_value,
+        p.actual_value,
+        i.expected_value - p.actual_value as difference_value,
+        case
+            when abs(i.expected_value - p.actual_value) < 0.01 then 'Match'
+            when (i.expected_value - p.actual_value) > 0 then 'Underpaid'
+            else 'Overpaid'
+        end as category
+    from
+    (
+        select order_id, sum(price + freight_value) as expected_value
+        from order_items
+        group by order_id
+    ) i
+    join
+    (
+        select order_id, sum(payment_value) as actual_value
+        from order_payments
+        group by order_id
+    ) p
+    on i.order_id = p.order_id
+)
+select * from recon;
